3cc58a7c03f01346535d75407b4c80959ce612ad
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-08-12 19:34:37 +0530

    applied relation operator

---
+++ relation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=25,b=16;
+    cout<<(a>=b)<<endl;
+    cout<<(a<=b)<<endl;
+    cout<<(a==b)<<endl;
+    cout<<(a!=b)<<endl;
+    return 0;
+
+}
